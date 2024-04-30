e65116412ae2c471d9ba6160fedb7d64843cb077
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-04-30 16:25:31 -0300

    DataSeason Record Class

---
+++ assets/TechCinema/src/main/java/br/com/alura/TechCinema/models/DataSeason.java
@@ -0,0 +1,10 @@
+package br.com.alura.TechCinema.models;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DataSeason(Integer Season,
+                         @JsonAlias("Episode") List<DataEpisode> dataEpisodes) {
+}
