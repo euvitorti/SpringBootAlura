bd1a70ec0510d2b94685c28bb86d4fd502242fb0
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-05-01 15:56:34 -0300

    Função LAMBDA

--- assets/TechCinema/src/main/java/br/com/alura/TechCinema/main/Main.java
+++ assets/TechCinema/src/main/java/br/com/alura/TechCinema/main/Main.java
@@ -2,6 +2,7 @@ package br.com.alura.TechCinema.main;
 
 import br.com.alura.TechCinema.api.Api;
 import br.com.alura.TechCinema.models.ConvertData;
+import br.com.alura.TechCinema.models.DataEpisode;
 import br.com.alura.TechCinema.models.DataSeason;
 import br.com.alura.TechCinema.models.DataSeries;
 
@@ -32,13 +33,23 @@ public class Main {
 
         List<DataSeason> seasons = new ArrayList<>();
 
-        for (int i = 1; i < dataSeries.totalSeasons(); i++) {
-			json = api.connect(ADDRESS + seriesName.replace(" ", "+") + "&season=" + i + API_KEY);
-			DataSeason dataSeason = convertData.getData(json, DataSeason.class);
-			seasons.add(dataSeason);
+        for (int i = 1; i <= dataSeries.totalSeasons(); i++) {
+            json = api.connect(ADDRESS + seriesName.replace(" ", "+") + "&season=" + i + API_KEY);
+            DataSeason dataSeason = convertData.getData(json, DataSeason.class);
+            seasons.add(dataSeason);
         }
 
-		seasons.forEach(System.out::println);
+        seasons.forEach(System.out::println);
+
+//        for (int i = 0; i < dataSeries.totalSeasons(); i++) {
+//            List<DataEpisode> dataEpisodes = seasons.get(i).dataEpisodes();
+//            for (int j = 0; j < dataEpisodes.size(); i++) {
+//                System.out.println(dataEpisodes.get(i).Title());
+//            }
+//        }
+
+        // LAMBDA
+        seasons.forEach(t -> t.dataEpisodes().forEach(e -> System.out.println(e.Title())));
     }
 
 }
