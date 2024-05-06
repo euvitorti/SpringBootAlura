9283df00b56971158f416f9204cf0f1041047db2
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-05-06 15:42:34 -0300

    Construtor

--- assets/TechCinema/src/main/java/br/com/alura/TechCinema/models/Serie.java
+++ assets/TechCinema/src/main/java/br/com/alura/TechCinema/models/Serie.java
@@ -1,4 +1,23 @@
 package br.com.alura.TechCinema.models;
 
+import java.util.OptionalDouble;
+
 public class Serie {
+    private String title;
+    private Integer totalSeasons;
+    private double imdbRating;
+    private Category genre;
+    private String actors;
+    private String poster;
+    private String plot;
+
+    public Serie(DataSeries dataSeries) {
+        this.title = dataSeries.Title();
+        this.totalSeasons = dataSeries.totalSeasons();
+        this.imdbRating = OptionalDouble.of(Double.valueOf(dataSeries.imdbRating())).orElse(0.0);
+        this.genre = Category.fromString(dataSeries.Genre().split(",")[0].trim());
+        this.actors = dataSeries.Actors();
+        this.poster = dataSeries.Poster();
+        this.plot = dataSeries.Plot();
+    }
 }
