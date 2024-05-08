84bb385688ef645c2baaf1a58e0a54ef34b9fd57
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-05-08 15:23:02 -0300

    Instância da interface Repository

--- assets/TechCinema/src/main/java/br/com/alura/TechCinema/TechCinemaApplication.java
+++ assets/TechCinema/src/main/java/br/com/alura/TechCinema/TechCinemaApplication.java
@@ -1,12 +1,17 @@
 package br.com.alura.TechCinema;
 
 import br.com.alura.TechCinema.main.Principal;
+import br.com.alura.TechCinema.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TechCinemaApplication implements CommandLineRunner {
+
+    @Autowired
+    private SerieRepository serieRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(TechCinemaApplication.class, args);
@@ -15,7 +20,7 @@ public class TechCinemaApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Principal principal = new Principal();
+        Principal principal = new Principal(serieRepository);
         principal.menu();
 
     }
