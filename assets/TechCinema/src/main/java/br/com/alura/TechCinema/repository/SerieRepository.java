4ac3b6fd6a9cc7aa8ace09541e02da3523f52845
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-05-13 14:37:46 -0300

    Query

--- assets/TechCinema/src/main/java/br/com/alura/TechCinema/repository/SerieRepository.java
+++ assets/TechCinema/src/main/java/br/com/alura/TechCinema/repository/SerieRepository.java
@@ -3,10 +3,16 @@ package br.com.alura.TechCinema.repository;
 import br.com.alura.TechCinema.models.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 //QUAL É ENTIDADE, E A CHAVE PRIMÁRIA
 // SALVANDO AS INFORMAÇÕES NO BANCO DE DADOS
 public interface SerieRepository extends JpaRepository<Serie, Long> {
+
     Optional<Serie> findByTitleContainsIgnoreCase(String serieName);
+
+    Optional<Serie> findByActorsContainsIgnoreCase(String actorName);
+
+    List<Serie> findTop5ByOrderByImdbRating();
 }
