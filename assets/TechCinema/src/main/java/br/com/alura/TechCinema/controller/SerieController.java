36fd106da7e558d07fe772e46c94bb7dc79d50e3
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-05-16 16:28:01 -0300

    Listando as séries no navegador

--- assets/TechCinema/src/main/java/br/com/alura/TechCinema/controller/SerieController.java
+++ assets/TechCinema/src/main/java/br/com/alura/TechCinema/controller/SerieController.java
@@ -1,14 +1,22 @@
 package br.com.alura.TechCinema.controller;
 
+import br.com.alura.TechCinema.models.Serie;
+import br.com.alura.TechCinema.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class SerieController {
 
+    @Autowired
+    private SerieRepository serieRepository;
+
     // DEFININDO A ROTA
     @GetMapping("/series")
-    public String getSerie() {
-        return "Welcome bro 😎";
+    public List<Serie> getSerie() {
+        return serieRepository.findAll();
     }
 }
