a3c2b697f81bc5de7460bf400779a6f70cb1ca37
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-05-16 14:04:39 -0300

    Utilizando o Spring Web

--- assets/TechCinema/src/main/java/br/com/alura/TechCinema/TechCinemaApplication.java
+++ assets/TechCinema/src/main/java/br/com/alura/TechCinema/TechCinemaApplication.java
@@ -1,27 +1,12 @@
 package br.com.alura.TechCinema;
 
-import br.com.alura.TechCinema.main.Principal;
-import br.com.alura.TechCinema.repository.SerieRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TechCinemaApplication implements CommandLineRunner {
-
-    @Autowired
-    private SerieRepository serieRepository;
+public class TechCinemaApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(TechCinemaApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-
-        Principal principal = new Principal(serieRepository);
-        principal.menu();
-
     }
 }
---
+++ assets/TechCinema/src/main/java/br/com/alura/TechCinema/TechCinemaApplicationWithOutWeb.java
@@ -0,0 +1,27 @@
+//package br.com.alura.TechCinema;
+//
+//import br.com.alura.TechCinema.main.Principal;
+//import br.com.alura.TechCinema.repository.SerieRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class TechCinemaApplicationWithOutWeb implements CommandLineRunner {
+//
+//    @Autowired
+//    private SerieRepository serieRepository;
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(TechCinemaApplicationWithOutWeb.class, args);
+//    }
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//
+//        Principal principal = new Principal(serieRepository);
+//        principal.menu();
+//
+//    }
+//}
