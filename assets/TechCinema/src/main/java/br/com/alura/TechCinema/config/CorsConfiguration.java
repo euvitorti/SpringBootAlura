0c63e7c0e2f17a0d35da60e9fc8ba0c010d02fbe
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-05-17 15:02:12 -0300

    Conectando ao front

--- assets/TechCinema/src/main/java/br/com/alura/TechCinema/config/CorsConfiguration.java
+++ assets/TechCinema/src/main/java/br/com/alura/TechCinema/config/CorsConfiguration.java
@@ -2,12 +2,15 @@ package br.com.alura.TechCinema.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfiguration {
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500")
+                .allowedOrigins("http://127.0.0.1:5501")
                 .allowedMethods("GET", "POST","PUT","DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
